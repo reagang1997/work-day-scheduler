5de485396baec2716b554c89e046e7ad046c7cad
ReaganG <rbgrunwald1997@gmail.com>
2021-01-11 10:02:32 -0600

    final

--- getDate.js
+++ getDate.js
@@ -1,32 +1,96 @@
-var months = ["January", "February", "March", "April" ]
+
 
 var date = moment().format("dddd, MMMM Do YYYY");
 
-var hour = moment().format("h");
+var hour = moment().format("H");
 
+
+if (localStorage.getItem("planner")) {
+    var planner = JSON.parse(localStorage.getItem("planner"));
+    $("input").filter(function(i, elem){
+        elem.value = planner[i].event;
+    })
+}
+else {
+    planner = [
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        },
+        {
+            event: ""
+        }
+    ]
+}
+
+
+for (var i = 0; i < planner.length; i++) {
+    console.log(planner[i]);
+}
 
 setDate();
 colorEvents();
 
 
-function setDate(){
+$("button").click(function () {
+    var hr = this.value;
+    console.log(hr);
+    var tmp = $("input").filter(function (i, elem) {
+        var tmpHr = $(elem).attr("data-hr");
+        if (tmpHr == hr) {
+            planner[hr - 9].event = elem.value;
+            console.log(planner[hr - 9].event);
+            console.log(planner);
+            localStorage.setItem("planner", JSON.stringify(planner));
+        }
+    })
+});
+
+function setDate() {
     $(".date").text(date);
 }
 
-function colorEvents(){
-    $(".events").each(function(i, elem){
+function colorEvents() {
+    $(".events").each(function (i, elem) {
         //var x = elem.attr("data-hr")
         var elemHour = $(elem).attr("data-hr")
-        if(+elemHour > +hour){
-            $(elem).addClass("future");
+
+        if (+elemHour > +hour) {
+            $(elem).attr("id", "future");
         }
-        else if(+elemHour < +hour){
-            $(elem).addClass("past");
+        else if (+elemHour < +hour) {
+            $(elem).attr("id", "past");
 
         }
-        else{
-            $(elem).addClass("current");
+        else {
+            $(elem).attr("id", "current");
 
         }
     })
 }
+
+
+function setLocal() {
+    localStorage.setItem("planner", planner);
+}
+
